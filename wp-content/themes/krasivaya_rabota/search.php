d3fe19b2e3fa554ee8bb2baa3102729bb8795b70
serg <aspirin_1988@mail.ru>
2016-05-19 16:59:22 +0600

    /start

--- wp-content/themes/krasivaya_rabota/search.php
+++ wp-content/themes/krasivaya_rabota/search.php
@@ -15,8 +15,8 @@ get_header(); ?>
 		<?php
 		if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'krasivaya_rabota' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+			<header class="page-header container">
+				<h1 class="page-title"><?php printf( esc_html__( 'Результаты поиска для : %s', 'krasivaya_rabota' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<?php
@@ -41,6 +41,7 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
-
+	<br>
+	<br>
 <?php
 get_footer();
--- wp-content/themes/krasivaya_rabota/template-parts/content-search.php
+++ wp-content/themes/krasivaya_rabota/template-parts/content-search.php
@@ -13,18 +13,18 @@
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php krasivaya_rabota_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
+<!--		--><?php //if ( 'post' === get_post_type() ) : ?>
+<!--		<div class="entry-meta">-->
+<!--			--><?php //krasivaya_rabota_posted_on(); ?>
+<!--		</div><!-- .entry-meta -->-->
+<!--		--><?php //endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
-	<footer class="entry-footer">
-		<?php krasivaya_rabota_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+<!--	<footer class="entry-footer">-->
+<!--		--><?php //krasivaya_rabota_entry_footer(); ?>
+<!--	</footer><!-- .entry-footer -->-->
 </article><!-- #post-## -->
