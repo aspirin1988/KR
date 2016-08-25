7bf80f3a609eb81b5ba37d0df5f8a7686f5bb2d4
Sergey Demidov <aspirin_1988@mail.ru>
2016-08-25 13:16:32 +0600

    /start

---
+++ wp-content/themes/krasivaya_rabota/searchform.php
@@ -0,0 +1,6 @@
+<form action="/">
+	<input name="s" type="text" id="searchQuery"
+		   placeholder="Поиск по сайту" class="form-control">
+	<button type="submit" class="btn-company-style"><span class="glyphicon glyphicon-search"></span>
+	</button>
+</form>
